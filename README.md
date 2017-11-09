b1e9674067bd25e5f657c2930544d909ab41cc6a
Chong Guo <armourg@sfu.ca>
2017-11-09 14:36:29 -0800

    updated README.md

--- README.md
+++ README.md
@@ -16,3 +16,4 @@
 * [x] inception
 * [x] resnet
 * [ ] densenet
+* [ ] gan
--- pytorch.py
+++ pytorch.py
@@ -134,154 +134,155 @@ def adjust_learning_rate(optimizer, epoch):
         param_group['lr'] = learning_rate
 
 
-# Setup args
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('-lr','--learning-rate', type=float, default=0.01,
-                    help='initial learning rate (default: 0.01)')
-parser.add_argument('-e', '--epochs', type=int, default=100,
-                    help='number of epochs to train (default: 100)')
-parser.add_argument('--train-batch-size', type=int, default=50,
-                    help='input batch size for training (default: 50)')
-parser.add_argument('--test-batch-size', type=int, default=100,
-                    help='input batch size for testing (default: 100)')
-parser.add_argument('--lr-decay-interval', type=int, default=50,
-                    help='number of epochs to decay the learning rate (default: 50)')
-parser.add_argument('--num-workers', type=int, default=4,
-                    help='number of workers (default: 4)')
-parser.add_argument('--momentum', type=float, default=0.9,
-                    help='SGD momentum (default: 0.9)')
-parser.add_argument('--seed', type=int, default=1,
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=50,
-                    help='how many batches to wait before logging training status (default: 50)')
-parser.add_argument('--save-interval', type=int, default=50,
-                    help='how many batches to wait before saving testing output image (default: 50)')
-parser.add_argument('-i1', '--enable-training-disturb-illumination', action='store_true', default=False,
-                    help='enable disturb illumination for traning data')
-parser.add_argument('-i2', '--enable-testing-disturb-illumination', action='store_true', default=False,
-                    help='enable disturb illumination for testing data')
-parser.add_argument('-l1', '--enable-training-log-transform', action='store_true', default=False,
-                    help='enable log transform for traning')
-parser.add_argument('-l2', '--enable-testing-log-transform', action='store_true', default=False,
-                    help='enable log transform for testing')
-parser.add_argument('-t', '--only-testing', action='store_true', default=False,
-                    help='only run testing')
-parser.add_argument('-r', '--resume', action='store_true', default=False,
-                    help='resume from checkpoint')
-args = parser.parse_args()
+if __name__ == '__main__':
+    # Setup args
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+    parser.add_argument('-lr','--learning-rate', type=float, default=0.01,
+                        help='initial learning rate (default: 0.01)')
+    parser.add_argument('-e', '--epochs', type=int, default=100,
+                        help='number of epochs to train (default: 100)')
+    parser.add_argument('--train-batch-size', type=int, default=50,
+                        help='input batch size for training (default: 50)')
+    parser.add_argument('--test-batch-size', type=int, default=100,
+                        help='input batch size for testing (default: 100)')
+    parser.add_argument('--lr-decay-interval', type=int, default=50,
+                        help='number of epochs to decay the learning rate (default: 50)')
+    parser.add_argument('--num-workers', type=int, default=4,
+                        help='number of workers (default: 4)')
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--seed', type=int, default=1,
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=50,
+                        help='how many batches to wait before logging training status (default: 50)')
+    parser.add_argument('--save-interval', type=int, default=50,
+                        help='how many batches to wait before saving testing output image (default: 50)')
+    parser.add_argument('-i1', '--enable-training-disturb-illumination', action='store_true', default=False,
+                        help='enable disturb illumination for traning data')
+    parser.add_argument('-i2', '--enable-testing-disturb-illumination', action='store_true', default=False,
+                        help='enable disturb illumination for testing data')
+    parser.add_argument('-l1', '--enable-training-log-transform', action='store_true', default=False,
+                        help='enable log transform for traning')
+    parser.add_argument('-l2', '--enable-testing-log-transform', action='store_true', default=False,
+                        help='enable log transform for testing')
+    parser.add_argument('-t', '--only-testing', action='store_true', default=False,
+                        help='only run testing')
+    parser.add_argument('-r', '--resume', action='store_true', default=False,
+                        help='resume from checkpoint')
+    args = parser.parse_args()
 
-# Init variables
-print('==> Init variables..')
-use_cuda = cuda.is_available()
-best_accuracy = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+    # Init variables
+    print('==> Init variables..')
+    use_cuda = cuda.is_available()
+    best_accuracy = 0  # best test accuracy
+    start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-# Init seed
-print('==> Init seed..')
-torch.manual_seed(args.seed)
-if use_cuda:
-    cuda.manual_seed(args.seed)
-
-# Download data
-print('==> Download data..')
-datasets.CIFAR100(root='data', train=True, download=True)
-
-# Calculate mean and std
-print('==> Calculate mean and std..')
-# mean_ori, std_ori = calculate_mean_and_std(enable_log=False)
-# print('mean_ori = ', mean_ori)
-# print('std_ori = ', std_ori)
-# mean_log, std_log = calculate_mean_and_std(enable_log=True)
-# print('mean_log = ', mean_log)
-# print('std_log = ', std_log)
-mean_ori, std_ori = (0.50707543, 0.48655024, 0.44091907), (0.26733398, 0.25643876, 0.27615029)
-mean_log, std_log = (6.69928741, 6.65900993, 6.40947819), (1.2056427,  1.15127575, 1.31597221)
-
-# Prepare training transform
-print('==> Prepare training transform..')
-traning_data_mean = torch.FloatTensor(mean_log) if args.enable_training_log_transform else torch.FloatTensor(mean_ori)
-traning_data_std = torch.FloatTensor(std_log) if args.enable_training_log_transform else torch.FloatTensor(std_ori)
-traning_transform = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-])
-if args.enable_training_disturb_illumination:
-    traning_transform = transforms.Compose([
-        traning_transform,
-        DisturbIllumination(),
-    ])
-if args.enable_training_log_transform:
-    traning_transform = transforms.Compose([
-        traning_transform,
-        LogSpace(),
-    ])
-traning_transform = transforms.Compose([
-    traning_transform,
-    transforms.Normalize(traning_data_mean, traning_data_std),
-])
-
-# Prepare testing transform
-print('==> Prepare testing transform..')
-testing_data_mean = torch.FloatTensor(mean_log) if args.enable_testing_log_transform else torch.FloatTensor(mean_ori)
-testing_data_std = torch.FloatTensor(std_log) if args.enable_testing_log_transform else torch.FloatTensor(std_ori)
-testing_transform = transforms.Compose([
-    transforms.ToTensor(),
-])
-if args.enable_testing_disturb_illumination:
-    testing_transform = transforms.Compose([
-        testing_transform,
-        DisturbIllumination(),
-    ])
-if args.enable_testing_log_transform:
-    testing_transform = transforms.Compose([
-        testing_transform,
-        LogSpace(),
-    ])
-testing_transform = transforms.Compose([
-    testing_transform,
-    transforms.Normalize(testing_data_mean, testing_data_std),
-])
-
-# Init dataloader
-print('==> Init dataloader..')
-trainset = datasets.CIFAR100(root='data', train=True, download=True, transform=traning_transform)
-trainloader = utils.data.DataLoader(trainset, batch_size=args.train_batch_size, shuffle=True, num_workers=args.num_workers)
-
-testset = datasets.CIFAR100(root='data', train=False, download=True, transform=testing_transform)
-testloader = utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, num_workers=args.num_workers)
-
-# Model
-print('==> Building model..')
-net = resnet_cifar.ResNet('res34', num_classes=100)
-# net = vgg.VGG('vgg16', num_classes=100)
-# net = alexnet.AlexNet(num_classes=100)
-# net = inception.InceptionV3(num_classes=100)
-if use_cuda:
-    net = net.cuda()
-
-# Resume if required
-if args.resume:
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    # Init seed
+    print('==> Init seed..')
+    torch.manual_seed(args.seed)
     if use_cuda:
-        checkpoint = torch.load('./checkpoint/ckpt.t7')
-    else:
-        checkpoint = torch.load('./checkpoint/ckpt.t7', map_location=lambda storage, loc: storage)
-    start_epoch = checkpoint['epoch']
-    best_accuracy = checkpoint['accuracy']
-    net.load_state_dict(checkpoint['state_dict'])
+        cuda.manual_seed(args.seed)
 
-# Loss function and Optimizer
-criterion = nn.CrossEntropyLoss()
-if use_cuda:
-    criterion = criterion.cuda()
-optimizer = optim.SGD(net.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=5e-4)
+    # Download data
+    print('==> Download data..')
+    datasets.CIFAR100(root='data', train=True, download=True)
 
-for epoch in range(start_epoch, start_epoch + args.epochs):
-    if args.only_testing:
-        test(epoch)
-    else:
-        adjust_learning_rate(optimizer, epoch)
-        train(epoch)
-        test(epoch)
+    # Calculate mean and std
+    print('==> Calculate mean and std..')
+    # mean_ori, std_ori = calculate_mean_and_std(enable_log=False)
+    # print('mean_ori = ', mean_ori)
+    # print('std_ori = ', std_ori)
+    # mean_log, std_log = calculate_mean_and_std(enable_log=True)
+    # print('mean_log = ', mean_log)
+    # print('std_log = ', std_log)
+    mean_ori, std_ori = (0.50707543, 0.48655024, 0.44091907), (0.26733398, 0.25643876, 0.27615029)
+    mean_log, std_log = (6.69928741, 6.65900993, 6.40947819), (1.2056427,  1.15127575, 1.31597221)
+
+    # Prepare training transform
+    print('==> Prepare training transform..')
+    traning_data_mean = torch.FloatTensor(mean_log) if args.enable_training_log_transform else torch.FloatTensor(mean_ori)
+    traning_data_std = torch.FloatTensor(std_log) if args.enable_training_log_transform else torch.FloatTensor(std_ori)
+    traning_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ])
+    if args.enable_training_disturb_illumination:
+        traning_transform = transforms.Compose([
+            traning_transform,
+            DisturbIllumination(),
+        ])
+    if args.enable_training_log_transform:
+        traning_transform = transforms.Compose([
+            traning_transform,
+            LogSpace(),
+        ])
+    traning_transform = transforms.Compose([
+        traning_transform,
+        transforms.Normalize(traning_data_mean, traning_data_std),
+    ])
+
+    # Prepare testing transform
+    print('==> Prepare testing transform..')
+    testing_data_mean = torch.FloatTensor(mean_log) if args.enable_testing_log_transform else torch.FloatTensor(mean_ori)
+    testing_data_std = torch.FloatTensor(std_log) if args.enable_testing_log_transform else torch.FloatTensor(std_ori)
+    testing_transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    if args.enable_testing_disturb_illumination:
+        testing_transform = transforms.Compose([
+            testing_transform,
+            DisturbIllumination(),
+        ])
+    if args.enable_testing_log_transform:
+        testing_transform = transforms.Compose([
+            testing_transform,
+            LogSpace(),
+        ])
+    testing_transform = transforms.Compose([
+        testing_transform,
+        transforms.Normalize(testing_data_mean, testing_data_std),
+    ])
+
+    # Init dataloader
+    print('==> Init dataloader..')
+    trainset = datasets.CIFAR100(root='data', train=True, download=True, transform=traning_transform)
+    trainloader = utils.data.DataLoader(trainset, batch_size=args.train_batch_size, shuffle=True, num_workers=args.num_workers)
+
+    testset = datasets.CIFAR100(root='data', train=False, download=True, transform=testing_transform)
+    testloader = utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, num_workers=args.num_workers)
+
+    # Model
+    print('==> Building model..')
+    net = resnet_cifar.ResNet('res34', num_classes=100)
+    # net = vgg.VGG('vgg16', num_classes=100)
+    # net = alexnet.AlexNet(num_classes=100)
+    # net = inception.InceptionV3(num_classes=100)
+    if use_cuda:
+        net = net.cuda()
+
+    # Resume if required
+    if args.resume:
+        print('==> Resuming from checkpoint..')
+        assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+        if use_cuda:
+            checkpoint = torch.load('./checkpoint/ckpt.t7')
+        else:
+            checkpoint = torch.load('./checkpoint/ckpt.t7', map_location=lambda storage, loc: storage)
+        start_epoch = checkpoint['epoch']
+        best_accuracy = checkpoint['accuracy']
+        net.load_state_dict(checkpoint['state_dict'])
+
+    # Loss function and Optimizer
+    criterion = nn.CrossEntropyLoss()
+    if use_cuda:
+        criterion = criterion.cuda()
+    optimizer = optim.SGD(net.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=5e-4)
+
+    for epoch in range(start_epoch, start_epoch + args.epochs):
+        if args.only_testing:
+            test(epoch)
+        else:
+            adjust_learning_rate(optimizer, epoch)
+            train(epoch)
+            test(epoch)
