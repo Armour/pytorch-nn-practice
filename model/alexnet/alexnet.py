d2c547b53aa7edff3a63f300854069db33acf650
Chong Guo <t-choguo@microsoft.com>
2018-07-06 14:47:27 -0700

    Fix(alexnet): fix wrong dropout layer order in alexnet model

--- model/alexnet/alexnet.py
+++ model/alexnet/alexnet.py
@@ -1,11 +1,19 @@
-#!/usr/bin/env python3
-# coding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Alexnet model."""
+
+__author__ = 'Chong Guo'
+__copyright__ = 'Copyright 2017, Chong Guo'
+__license__ = 'MIT'
+__email__ = 'armourcy@email.com'
 
 import math
 import torch.nn as nn
 
+
 class AlexNet(nn.Module):
-    """ Alexnet model """
+    """Alexnet model."""
     def __init__(self, num_classes=1000):
         """ Init
         Args:
@@ -15,27 +23,22 @@ class AlexNet(nn.Module):
         self.features = nn.Sequential(
             # [n, 3, 224, 224]
             nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
-            # [n, 96, 55, 55]
             nn.ReLU(inplace=True),
             # [n, 96, 55, 55]
             nn.MaxPool2d(kernel_size=3, stride=2),
             # [n, 96, 27, 27]
             nn.Conv2d(96, 256, kernel_size=5, padding=2),
-            # [n, 256, 27, 27]
             nn.ReLU(inplace=True),
             # [n, 256, 27, 27]
             nn.MaxPool2d(kernel_size=3, stride=2),
             # [n, 256, 13, 13]
             nn.Conv2d(256, 384, kernel_size=3, padding=1),
-            # [n, 384, 13, 13]
             nn.ReLU(inplace=True),
             # [n, 384, 13, 13]
             nn.Conv2d(384, 384, kernel_size=3, padding=1),
-            # [n, 384, 13, 13]
             nn.ReLU(inplace=True),
             # [n, 384, 13, 13]
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            # [n, 256, 13, 13]
             nn.ReLU(inplace=True),
             # [n, 256, 13, 13]
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -43,17 +46,13 @@ class AlexNet(nn.Module):
         )
         self.classifier = nn.Sequential(
             # [n, 256 * 6 * 6]
+            nn.Dropout(p=0.5),
             nn.Linear(256 * 6 * 6, 4096),
-            # [n, 4096]
             nn.ReLU(inplace=True),
             # [n, 4096]
-            nn.Dropout(),
-            # [n, 4096]
+            nn.Dropout(p=0.5),
             nn.Linear(4096, 4096),
-            # [n, 4096]
             nn.ReLU(inplace=True),
-            # [n, 4096]
-            nn.Dropout(),
             # [n, 4096]
             nn.Linear(4096, num_classes),
             # [n, num_classes]
@@ -86,4 +85,6 @@ if __name__ == "__main__":
     sample_data = torch.ones(12, 3, 224, 224)
     sample_input = Variable(sample_data)
     net = AlexNet()
+    print(net)
     print(net(sample_input))
+    print(net(sample_input).shape)
